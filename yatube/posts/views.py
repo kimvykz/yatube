6d4596b60f6584a5cb8bd200b3ac6214728ef8a4
kimvykz <kimslavakz@gmail.com>
2023-01-25 15:08:18 +0600

    beta 0004

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+def index(request):
+    latest = Post.objects.order_by('-pub_date')[:11]
+    #output = []
+    #for item in latest:
+    #    output.append(item.text)
+    #return HttpResponse('\n'.join(output))    
+    return render(request, "index.html", {"posts" : latest})
+
+#def my_index(request):
+#    return render(request,
+#                  'index.html',
+#                  {'title': 'Этот текст встанет в шаблон на место переменной "title"'}
+#                  )    
 
 # Create your views here.
